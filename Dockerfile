be41adb48305eacb85b33bd5885b03f2328aceda
Pavlo <suhonos.p@gmail.com>
2022-01-11 14:43:39 +0200

    Changes for Heroku

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # start by pulling the python image
-FROM python:3.10-alpine
+FROM python:3.10.1
 
 RUN mkdir -p /app
 WORKDIR /app
--- app.py
+++ app.py
@@ -39,6 +39,5 @@ def all_names():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0')
-    app.run( host='0.0.0.0', port=port)
+    app.run( host='0.0.0.0', port=5000)
