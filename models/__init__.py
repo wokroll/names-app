52098f9582a559941c7d90cefb67abe937f383f4
Pavlo <suhonos.p@gmail.com>
2022-01-11 14:56:30 +0200

    Fixed default datetime value for created_at

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 from db import db
 import datetime
+from sqlalchemy.sql import func
 
 
 class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), nullable=False, unique=True)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    created_at = db.Column(db.DateTime, default=func.now())
